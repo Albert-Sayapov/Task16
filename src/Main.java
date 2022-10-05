03267a6c3dfda27bab082b530c7b2acdfe2026b8
Albert Sayapov <asayapov@mail.ru>
2022-10-05 13:16:17 +0700

    Fix bonus counts

--- src/Main.java
+++ src/Main.java
@@ -38,31 +38,31 @@ public class Main {
 
         System.out.println("Ваша корзина покупок:");
         int sum = 0;
-        for (int i = 0; i < products.length; i++) {
-            sum += prices[i] * counts[i];
-        }
-        boolean doBonus = sum >= MIN_COST_FOR_BONUS;
+
         for (int i = 0; i < products.length; i++) {
             if (counts[i] != 0) {
-                if (doBonus == true) {
-                    counts[i] += 1;
-                }
-                
+
                 boolean isOnSale = false;
                 for (String saleProduct : productsOnSale) {
                     if (products[i].equals(saleProduct)) {
                         isOnSale = true;
                     }
                 }
-
                 if (isOnSale) {
-                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * (counts[i] / 3 * 2 + counts[i] % 3)) + " руб. (распродажа!)");
                     sum += prices[i] * (counts[i] / 3 * 2 + counts[i] % 3);
-                } else {
-                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * counts[i]) + " руб.");
-                    sum += prices[i] * counts[i];
-                }
+                    if (sum >= MIN_COST_FOR_BONUS) {
+                        counts[i] += 1;
+                    }
+                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + sum + " руб. (распродажа!)");
 
+                } else {
+                    sum += prices[i] * counts[i];
+                    if (sum >= MIN_COST_FOR_BONUS) {
+                        counts[i] += 1;
+                    }
+                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + sum + " руб.");
+
+                }
             }
         }
         System.out.println("Итого: " + sum + " руб.");
